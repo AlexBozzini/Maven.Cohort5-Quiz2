51fc4acb2685f72d74136ca032b0aa0431087774
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-02-27 14:58:37 -0500

    First attempt arrayutils

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -2,18 +2,52 @@ package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+        Integer[] mergedArray = new Integer[array1.length + array2.length];
+        for (int i = 0; i < array1.length; i++) {
+            mergedArray[i] = array1[i];
+        }
+        for (int i = 0; i < array2.length; i++) {
+            mergedArray[i + array1.length] = array2[i];
+        }
+        return mergedArray;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
+        Integer counter = 0;
+        while (counter <= index) {
+
+        }
         return null;
     }
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
-        return null;
+        Integer[] mergedArray = merge(array1, array2);
+        Integer counter = 0;
+        for (int i = 0; i < mergedArray.length; i++) {
+            if (mergedArray[i] == valueToEvaluate) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public Integer mostCommon(Integer[] array) {
-        return null;
+        Integer count = 1, tempCount;
+        Integer mode = array[0];
+        Integer temp;
+        for (int i = 0; i < array.length - 1; i++) {
+            temp = array[i];
+            tempCount = 0;
+            for (int j = 1; j < array.length; j++) {
+                if (temp == array[j]) {
+                    tempCount++;
+                }
+            }
+            if (tempCount > count) {
+                mode = temp;
+                count = tempCount;
+            }
+        }
+        return mode;
     }
 }
