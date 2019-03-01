4ec7213356cbb184265e41c8da9a077333ec8458
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-01 15:28:47 -0500

    finished router, except for toString method

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,26 +1,35 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Router {
+    Map<String, String> router = new HashMap<>();
     public void add(String path, String controller) {
-
-
+        router.putIfAbsent(path, controller);
     }
 
     public Integer size() {
 
-        return null;
+        return router.size();
     }
 
     public String getController(String path) {
 
-        return null;
+        return router.get(path);
     }
 
     public void update(String path, String studentController) {
+        router.put(path, studentController);
     }
 
     public void remove(String path) {
+        router.remove(path);
     }
-}
+
+    public String toString(){
+        String output = "";
+        return output;
+        }
+    }
